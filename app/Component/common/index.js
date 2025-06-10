887c040d74ead4400e5203fd5982302c33d504a4
Manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-06-10 11:25:17 +0530

    add-imfwr

--- app/Component/common/index.js
+++ app/Component/common/index.js
@@ -6,4 +6,3 @@ export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
 export const storageLink = 'http://localhost:8000/storage'
  
 export const development= "roduction";
-//https://api.tringaa.com/  
