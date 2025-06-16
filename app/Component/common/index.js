14b40d9938fffa1ead457e3548e8975769b90448
Manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-06-16 16:59:16 +0530

    add-lives-links

--- app/Component/common/index.js
+++ app/Component/common/index.js
@@ -1,8 +1,8 @@
-export const apilink = 'http://localhost:8000/api/v1'
+export const apilink = 'http://admin.nextgentrip.com/api/v1'
 // export const apilink = 'http://api.tringaa.com/api/v1'
 
 export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
 // export const storageLink = 'http://api.tringaa.com/storage'
-export const storageLink = 'http://localhost:8000/storage'
+export const storageLink = 'http://admin.nextgentrip.com/storage'
  
 export const development= "roduction";
